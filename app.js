faa39b12f533876aed67f637774f1a58175666de
Nabha2004 <nabhabagwan32@gmail.com>
2025-03-01 22:30:37 +0530

    Added function to generate computer's choice

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-let userScore=0;
-let compScore=0;
+let userScore = 0;
+let compScore = 0;
 
-const choices=document.querySelectorAll(".choice");
+const choices = document.querySelectorAll(".choice");
 
-const genCompChoice = ()=> {
-    const options = ["rock","paper","scissors"];
-    const ranIdx=Math.floor(Math.random() * 3);
+const genCompChoice = () => {
+    const options = ["rock", "paper", "scissors"];
+    const ranIdx = Math.floor(Math.random() * 3);
     return options[ranIdx];
-}
+};
 
-choices.forEach((choice)=>{
-    choice.addEventListener("click",()=>{
-        
-    })
-})
-
-
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.id.toLowerCase();
+        const compChoice = genCompChoice();
+        console.log(userChoice, compChoice);
+    });
+});
